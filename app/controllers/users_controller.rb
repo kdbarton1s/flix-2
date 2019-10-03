206b1c3107717baed2b9377cac87a896a10ad4f1
kayla <kdbarton1s@gmail.com>
2019-10-03 15:07:23 -0500

    Added sign out function and authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :require_signin, except: [:new, :create]
+  before_action :requre_correct_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -23,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "User successfully updated"
     else
@@ -36,8 +37,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
     redirect_to root_url, alert: "Account successfully deleted."
   end
 
@@ -47,5 +48,13 @@ private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :username)
+  end
+
+  def requre_correct_user
+    @user = User.find(params[:id])
+    unless current_user?(@user)
+      redirect_to root_url
+    end
+
   end
 end
